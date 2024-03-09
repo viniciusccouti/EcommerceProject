3378a0060c59a9d29cafb8acda83f279eedb23e8
Vinicius de Castro Coutinho <viniciuscoutinho_@hotmail.com>
2024-03-09 15:50:47 -0300

    09-03.02

--- loja/models.py
+++ loja/models.py
@@ -10,11 +10,10 @@ class Cliente(models.Model):
     id_sessao = models.CharField(max_length=200, null=True, blank=True)
     usuario = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 
-
 class Categoria(models.Model): # Categorias (Masculino, Feminino, Infantil)
     nome = models.CharField(max_length=200, null=True, blank=True)
 
-class Tipo(models.Model) # Tipos (Camisa, Camiseta, Bermuda, Calca)
+class Tipo(models.Model): # Tipos (Camisa, Camiseta, Bermuda, Calca)
     nome = models.CharField(max_length=200, null=True, blank=True)
 
 class Produto(models.Model):
@@ -23,36 +22,34 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     ativo = models.BooleanField(default=True)
     categoria = models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.SET_NULL)
-    tipo = models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.SET_NULL)
+    tipo = models.ForeignKey(Tipo, null=True, blank=True, on_delete=models.SET_NULL)
    
 class ItemEstoque(models.Model):
     produto = models.ForeignKey(Produto, null=True, blank=True, on_delete=models.SET_NULL)
     cor = models.CharField(max_length=200, null=True, blank=True)
     tamanho = models.CharField(max_length=200, null=True, blank=True)
     quantidade = models.IntegerField(default=0)
+    
+class Endereco(models.Model):
+    rua = models.CharField(max_length=400, null=True, blank=True)
+    numero = models.IntegerField(default=0)
+    complemento = models.CharField(max_length=200, null=True, blank=True)
+    cep = models.CharField(max_length=200, null=True, blank=True)
+    cidade = models.CharField(max_length=200, null=True, blank=True)
+    estado = models.CharField(max_length=200, null=True, blank=True)
+    cliente = models.ForeignKey(Cliente, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente, null=True, blank=True, on_delete=models.SET_NULL)
+    finalizado = models.BooleanField(default=False)
+    codigo_transacao = models.CharField(max_length=200, null=True, blank=True)
+    endereco = models.ForeignKey(Endereco, null=True, blank=True, on_delete=models.SET_NULL)
+    data_finalizacao = models.DateTimeField(null=True, blank=True)
    
 class ItensPedido(models.Model):
     item_estoque = models.ForeignKey(ItemEstoque, null=True, blank=True, on_delete=models.SET_NULL)
     quantidade = models.IntegerField(default=0)
-    pedido = models
+    pedido = models.ForeignKey(Pedido, null=True, blank=True, on_delete=models.SET_NULL)
 
-# ItensPedido
-    # itemestoque (camisa, laranja, M)
-    # quantidade (10 itens)
-    
-# Pedido
-    # cliente
-    # data_finalizacao
-    # finalizado
-    # id_transacao
-    # endereco
-    # itenspedido
 
-# Endereco
-    # rua
-    # numero
-    # complemento
-    # cep
-    # cidade
-    # estado
-    # cliente
