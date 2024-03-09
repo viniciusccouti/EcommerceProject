1920b9a3f6c203f33dbe1733920382ba8a51424c
Vinicius de Castro Coutinho <viniciuscoutinho_@hotmail.com>
2024-03-09 15:01:50 -0300

    09-03

--- loja/models.py
+++ loja/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    telefone = models.CharField(max_length=200, null=True, blank=True)
+    id_sessao = models.CharField(max_length=200, null=True, blank=True)
+    usuario = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+
+
+class Categoria(models.Model): # Categorias (Masculino, Feminino, Infantil)
+    nome = models.CharField(max_length=200, null=True, blank=True)
+
+class Tipo(models.Model) # Tipos (Camisa, Camiseta, Bermuda, Calca)
+    nome = models.CharField(max_length=200, null=True, blank=True)
+
+class Produto(models.Model):
+    imagem = models.CharField(max_length=400, null=True, blank=True)
+    nome = models.CharField(max_length=200, null=True, blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    ativo = models.BooleanField(default=True)
+    categoria = models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.SET_NULL)
+    tipo = models.ForeignKey(Categoria, null=True, blank=True, on_delete=models.SET_NULL)
+   
+class ItemEstoque(models.Model):
+    produto = models.ForeignKey(Produto, null=True, blank=True, on_delete=models.SET_NULL)
+    cor = models.CharField(max_length=200, null=True, blank=True)
+    tamanho = models.CharField(max_length=200, null=True, blank=True)
+    quantidade = models.IntegerField(default=0)
+   
+class ItensPedido(models.Model):
+    item_estoque = models.ForeignKey(ItemEstoque, null=True, blank=True, on_delete=models.SET_NULL)
+    quantidade = models.IntegerField(default=0)
+    pedido = models
+
+# ItensPedido
+    # itemestoque (camisa, laranja, M)
+    # quantidade (10 itens)
+    
+# Pedido
+    # cliente
+    # data_finalizacao
+    # finalizado
+    # id_transacao
+    # endereco
+    # itenspedido
+
+# Endereco
+    # rua
+    # numero
+    # complemento
+    # cep
+    # cidade
+    # estado
+    # cliente
