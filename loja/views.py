1312b5e8c0f274c57e470f338938e4b2e819e861
Vinicius de Castro Coutinho <viniciuscoutinho_@hotmail.com>
2024-03-13 19:46:05 -0300

    ordem

--- loja/views.py
+++ loja/views.py
@@ -3,10 +3,12 @@ from .models import *
 
 # Create your views here.
 def homepage(request):
-    return render(request,'homepage.html')
+    banners = Banner.objects.filter(ativo=True)
+    context = {"banners": banners}
+    return render(request,'homepage.html', context)
 
 def loja(request):
-    produtos = Produto.objects.all() 
+    produtos = Produto.objects.filter(ativo=True) 
     context = {"produtos": produtos}
     return render(request,'loja.html', context)
 
